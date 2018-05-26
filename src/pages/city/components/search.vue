44b08ed6c93d13ed2b89b152b659191a01f8783e
flyFatSea <491927227@qq.com>
2018-05-26 19:42:00 +0800

    city-search-logic finished

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,17 +1,84 @@
 <template>
 	<div class="search">
-		<input class="SearchInput" type="text" placeholder="输入城市名或拼音">
+		<input v-model="keyWord" class="SearchInput" type="text" placeholder="输入城市名或拼音">
+		<div class="search-content"
+		v-show="hasList"
+		ref="searchList"
+		>
+			<ul>
+				<li
+				class="item-list border-topbottom"
+				v-for="item of list"
+				:key="item.id"
+				@click="handleLiClick"
+				ref="searchLi"
+				>{{item.name}}</li>
+				<li
+				class="item-list border-topbottom"
+				v-show="hasDate">
+					没有匹配的数据项
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-	name: 'CitySearch'
+	name: 'CitySearch',
+	props:{
+		cities:Object
+	},
+	data () {
+		return {
+			keyWord: '',
+			list: [],
+			timer: null
+		}
+	},
+	methods: {
+		handleLiClick (e) {
+			this.keyWord = e.target.innerText
+		}
+	},
+	computed: {
+		hasList () {
+			return this.keyWord
+		},
+		hasDate () {
+			return !this.list.length
+		}
+	},
+	mounted () {
+		this.scroll=new BScroll(this.$refs.searchList)
+	},
+	watch: {
+		keyWord () {
+			if (this.timer) {
+				clearTimeout(this.timer)
+			}
+			this.timer = setTimeout(() =>{
+				const result = []
+				for (let i in this.cities) {
+					this.cities[i].forEach((value) =>{
+						if (value.spell.indexOf(this.keyWord) > -1 || value.name.indexOf(this.keyWord) > -1) {
+							result.push(value)
+						}
+					})
+				}
+				this.list = result
+			},100)
+		}
+	}
 }
 </script>
 
 <style lang="stylus" scoped>
 	@import '~styles/varibies.styl'
+	.border-topbottom
+		&:before
+			border-color:#ccc
 	.search
 		height:.72rem
 		background:$bgColor
@@ -24,4 +91,23 @@ export default {
 			border-radius:.05rem
 			padding:0 0.1rem
 			box-sizing:border-box
+	.search-content
+		overflow:hidden
+		position:absolute
+		top:1.58rem
+		right:0
+		bottom:0
+		left:0
+		background:#ccc
+		z-index:1
+		.item-list
+			overflow:hidden
+			height:.5rem
+			line-height:.5rem
+			padding-left:.2rem
+			color:#666
+			background:#fff
+			
+			
+			
 </style>
